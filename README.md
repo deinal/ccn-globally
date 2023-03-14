0ab8ea11ff2d6a93ab3d5731cdb12b41cb333f53
deinal <daniel.holmberg97@gmail.com>
2023-03-14 17:38:59 +0200

    Animate N100 predictions in May 22

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 
 ```
 conda create -n n100 python=3.7.0
+conda activate n100
 conda install -c conda-forge py-xgboost-gpu -y
 conda install -c conda-forge jupyterlab matplotlib -y
 conda install -c anaconda pandas jupyter -y
@@ -14,6 +15,7 @@ conda install -c anaconda pandas jupyter -y
 
 ```
 conda create -n geo
+conda activate geo
 conda install -c conda-forge xarray xgboost cartopy cfgrib
 pip install gif
 ```
--- global_plot.py
+++ global_plot.py
@@ -4,20 +4,18 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import LogFormatter
+from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 import gif
 
 
-features = ['co', 'c5h8', 'no', 'no2', 'so2']
-
-# Prepare data
-ds = xr.open_dataset('data/global.grib', engine='cfgrib')
-
-
 @gif.frame
-def plot_frame(i, t):
-    df = ds.isel(time=i).to_dataframe()
+def plot_frame(ds, date, time):
+
+    # Load & preprocess data
+    df = ds.to_dataframe()
+    features = ['co', 'c5h8', 'no', 'no2', 'so2']
     for v in features:
         df[v] = np.log(df[v].where(df[v] > 0, df[v][df[v] > 0].min()))
 
@@ -33,30 +31,38 @@ def plot_frame(i, t):
     result['n100'] = result.n100.assign_attrs(long_name='Conc.')
 
     # Plot result
-
+    vmin, vmax = 5.6, 6760
     major_ticks = [10, 100, 1000, 10000]
     fig, ax = plt.subplots(nrows=1, subplot_kw={'projection': ccrs.Robinson()})
     earth = result.n100.plot.contourf(
-        ax=ax, transform=ccrs.PlateCarree(), 
-        levels=np.geomspace(4.85, 8170, 30), norm=LogNorm(), cmap='viridis', extend='neither',
+        ax=ax, transform=ccrs.PlateCarree(), levels=np.geomspace(vmin, vmax, 30), 
+        norm=LogNorm(vmin=vmin, vmax=vmax), cmap='viridis', extend='neither',
         cbar_kwargs={'fraction': 0.03, 'ticks': major_ticks}    
     )
     earth.colorbar.ax.minorticks_on()
-    ax.set_title(f'Global N100, 2022-05-01T{t}')
+    ax.set_title(f'Global N100, {date}T{time}')
     ax.set_global()
     ax.coastlines()
 
     plt.tight_layout()
-    plt.savefig(f'figures/frame_{i}.png')
+    plt.savefig(f'figures/frame_{date}_{time}.png')
 
 
 frames = []
 times = ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00']
 
-for i in range(8):
-    print(times[i])
-    frame = plot_frame(i, times[i])
-    frames.append(frame)
+start_date = datetime(2022, 5, 1)
+end_date = datetime(2022, 5, 31)
+delta = timedelta(days=1)
+
+while start_date <= end_date:
+    current_date = str(start_date.date())
+    for i, time in enumerate(times):
+        # Prepare data
+        ds = xr.open_dataset(f'data/global/{current_date}.grib', engine='cfgrib')
+        frame = plot_frame(ds.isel(time=i), current_date, time)
+        frames.append(frame)
+        
+    start_date += delta
+
 gif.save(frames, 'figures/global_ccn.gif', duration=150)
-
-
